399866b7e2c663e995b36fe9127808176d995f00
Matthias <matthias.poortvliet@kpn.com>
2023-03-04 10:29:06 +0100

    updated cors settings

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,3 +1,5 @@
+console.log(process.env.GQL_SERVER)
+
 export default defineNuxtConfig({
   runtimeConfig: {
     secret: process.env.TOKEN_SECRET,
@@ -14,7 +16,7 @@ export default defineNuxtConfig({
       default: {
         authType: 'Bearer',
         authHeader: 'Authorization',
-        httpEndpoint: process.env.GQL_SERVER || 'http://localhost:3000/gql',
+        httpEndpoint: process.env.GQL_SERVER as string,
         tokenName: 'momo:token',
         tokenStorage: 'cookie'
       }
